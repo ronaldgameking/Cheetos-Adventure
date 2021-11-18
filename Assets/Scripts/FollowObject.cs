3bc08b0c1de1f5d5220802008c1a4f8848e4837b
Ronald <kpn.nl.rs@gmail.com>
2021-11-18 17:42:30 +0100

    Player movement
    Unsafe code errors can be ignored

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Alias using
+using UnityUtils;
 
 public class FollowObject : MonoBehaviour
 {
@@ -14,44 +17,113 @@ public class FollowObject : MonoBehaviour
         XZ = 0b1010,
         YZ = 0b0110
     }
-
     public Transform followThis;
     public Vector3 offset;
 
     [Header("Constrains")]
     public LockConstrains Position;
-    // Update is called once per frame
+    public LockConstrains NoOffset;
+
+    private bool offsetXDone = false;
+    private bool offsetYDone = false;
+    private bool offsetZDone = false;
+
+    private Vector3 newPos = Vector3.zero;
+    private IntegerBoolean offsetAppliedLock = new IntegerBoolean();
+
+    private void Awake()
+    {
+        //Don't judge me, I like pointers...
+        #region pointers
+#if UNITY_EDITOR
+        //Make a struct (pass by value) act like a class (pass by reference)
+        unsafe
+        {
+            //Make a new instance of struct
+            IntegerBoolean ib = new IntegerBoolean();
+            //Get the memory address of the struct
+            IntegerBoolean* _ibptr = &ib;
+            //Assign some number
+            _ibptr->getMeViaPointer = 298364;
+            //Make another var point to that address
+            IntegerBoolean* ibsec = _ibptr;
+            //Output current value of _ibptr's getMeViaPointer
+            Debug.Log(_ibptr->getMeViaPointer);
+            //Change the value of _ibptr's variable
+            _ibptr->getMeViaPointer = 298000;
+            //Show that the value has changed for both
+            Debug.Log(_ibptr->getMeViaPointer);
+            Debug.Log(ibsec->getMeViaPointer);
+        }
+#endif
+        #endregion
+    }
+
     void Update()
     {
-        Debug.Log(System.Convert.ToString((int)Position, 2));
-        LockConstrains _con = LockConstrains.OnlyX | LockConstrains.OnlyY | LockConstrains.OnlyZ;
-        Debug.Log(System.Convert.ToString((int)_con, 2));
-
+        //Some debugging stuff, ignore
+        //Show signed 32 bit integer as binary (base 2)
+        Debug.Log(Convert.ToString((int)Position, 2));
+        LockConstrains _con = LockConstrains.YZ;
+        Debug.Log(Convert.ToString((int)_con, 2));
+        newPos = transform.position;
         try
         {
-            if (((int)Position & 0b1110) == 0b0000)
+            //Check if X shouldn't be changed by extracting the bits.
+            //We need the binary AND operator then comparing it against binary 0
+            //if lock enabled comparison evaluates to false
+            if (((int)Position & 0b1000) == 0b0000)
             {
-                transform.position = followThis.position + offset;
+                newPos.x = followThis.position.x + offset.x;
+            }
+            //Check if X should still have an offset applied
+            else if (((int)NoOffset & 0b1000) == 0b0000 && !offsetXDone)
+            {
+                newPos.x = transform.position.x + offset.x;
+                offsetXDone = true;
+                //offsetAppliedLock = new IntegerBoolean(true,  offsetAppliedLock.Evaluate(1), offsetAppliedLock.Evaluate(2), false);
             }
 
-            else
+            //Same as X but with Y
+            if (((int)Position & 0b0100) == 0b0000)
             {
-                Vector3 curpos = transform.position;
-                curpos.y = followThis.position.y + offset.y;
-                curpos.z = followThis.position.z + offset.z;
-                transform.position = curpos;
-
+                newPos.y = followThis.position.y + offset.y;
+            }
+            else if (((int)NoOffset & 0b0010) == 0b0000 && !offsetYDone)
+            {
+                newPos.y = transform.position.y + offset.y;
+                offsetYDone = true;
+                //offsetAppliedLock = new IntegerBoolean(offsetAppliedLock.Evaluate(0), true, offsetAppliedLock.Evaluate(2), false);
             }
 
-            switch ((int)Position & 0b1110)
+            //Same as X and Y but with Z
+            if (((int)Position & 0b0010) == 0b0000)
             {
-                default:
-                    break;
+                newPos.z = followThis.position.z + offset.z;
             }
+            else if (((int)NoOffset & 0b0010) == 0b0000 && !offsetZDone)
+            {
+                SetZ();
+                //newPos.z = transform.position.z + offset.z;
+                offsetZDone = true;
+                //offsetAppliedLock = new IntegerBoolean(offsetAppliedLock.Evaluate(0), offsetAppliedLock.Evaluate(1), true,  false);
+            }
+
+            transform.position = newPos;
+            newPos = Vector3.zero;
         }
-        catch
+        catch (Exception e)
         {
+            Debug.LogError(e);
             enabled = false;
         }
+    }
+
+    private void SetZ()
+    {
+        Vector3 pos = transform.position;
+        pos.z = transform.position.z + offset.z;
+        Debug.Log(transform.position.z + "|" + pos.z + "|" + offset.z);
+        transform.position = pos;
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,3 @@
-//
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,60 +9,49 @@ public class PlayerMovement : MonoBehaviour
     public int CurrentLane = 1;
     public float Speed = 20f;
 
+    //https://answers.unity.com/questions/1342068/increase-speed-from-0f-10f-over-time.html
+    public float MovementSpeed = 5.0f;
+    public GameObject Player;
+    public float accelerationTime = 60;
+    // New variables :
+    private float currentSpeed = 1f;
+    private float maxSpeed = 14f;
+    private float screenCenterX;
+    private float minSpeed;
+    private float time;
+    //end
     private void Awake()
     {
         if (rb == null) rb = GetComponent<Rigidbody2D>();
-    }
-    //private void Update()
-    //{
-    //    if (Input.GetKeyUp(KeyCode.W))
-    //    {
-    //        if (CurrentLane > 0)
-    //        {
-    //            CurrentLane--;
-    //            Vector3 pos = transform.position;
-    //            pos.y = Lanes[CurrentLane].position.y;
-    //            transform.position = pos;
-    //        }
-    //    }
-    //    else if (Input.GetKeyUp(KeyCode.S))
-    //    {
-    //        if (CurrentLane < 2)
-    //        {
-    //            CurrentLane++;
-    //            Vector3 pos = transform.position;
-    //            pos.y = Lanes[CurrentLane].position.y;
-    //            transform.position = pos;
-    //        }
-    //    }
-    //    rb.velocity = Vector2.right * Speed * Time.deltaTime;
-    //}
-
-    float currentSpeed = 0f;
-    float maxSpeed = 10f;
-    public float movementSpeed = 5.0f;
-    public GameObject player;
-    private float screenCenterX;
-    // New variables :
-    public float accelerationTime = 60;
-    private float minSpeed;
-    private float time;
-
-    private void Start()
-    {
-        // save the horizontal center of the screen
         screenCenterX = Screen.width * 0.5f;
         minSpeed = currentSpeed;
         time = 0;
     }
-
-
     private void Update()
     {
-        // https://docs.unity3d.com/ScriptReference/Mathf.SmoothStep.html
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (CurrentLane > 0)
+            {
+                CurrentLane--;
+                Vector3 pos = transform.position;
+                pos.y = Lanes[CurrentLane].position.y;
+                transform.position = pos;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            if (CurrentLane < 2)
+            {
+                CurrentLane++;
+                Vector3 pos = transform.position;
+                pos.y = Lanes[CurrentLane].position.y;
+                transform.position = pos;
+            }
+        }
         currentSpeed = Mathf.SmoothStep(minSpeed, maxSpeed, time / accelerationTime);
         transform.position -= -transform.right * currentSpeed * Time.deltaTime;
         time += Time.deltaTime;
-        // ....
+        //rb.velocity = Vector2.right * Speed * Time.deltaTime;
     }
 }
