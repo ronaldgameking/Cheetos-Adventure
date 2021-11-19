6372b9d79742c4a7a009cb482638a6a9a35f16e2
Ronald <kpn.nl.rs@gmail.com>
2021-11-19 10:20:19 +0100

    Revert "Merge branch 'Ronald' into main"
    This reverts commit 2ff59dcf0d8aa65b7226923af766e145f0f9c82b.

--- Assets/Scripts/Util/UnityGame_utils/EventDelegates.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-namespace UnityUtils.delegates
-{
-    public delegate void SimpleCallback();
-    public delegate bool EvaluationHandle();
-    public delegate void ReferencedCallback(GameObject _go);
-}
